1c87edb3863547b6d0c48d56e85c62af9f698fba
sujonphero <sujonsheikh.in@gmail.com>
2024-06-03 10:58:18 +0600

    header section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/shared/Header";
+import About from "./pages/About";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="" element={<Home/>} >
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 };

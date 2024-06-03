ae9e2e6862d6d237c931ac03b31906cac9ad2e0d
sujonphero <sujonsheikh.in@gmail.com>
2024-06-04 00:29:28 +0600

    signup done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,130 @@
+import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  // handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+        if (
+          data.message.includes("11000") &&
+          data.message.includes("username")
+        ) {
+          toast.error("Username already exists.");
+        } else if (data.message.includes("email")) {
+          toast.error("user email must be unique");
+        } else if (data.message.includes("NetworkError")) {
+          toast.error("Network error. Please try again.");
+        } else {
+          toast.error("An error occurred. Please try again.");
+        }
+        return;
+      }
+      setError(null);
+      toast.success("Sign up successful!");
+
+      setFormData({});
+      setTimeout(() => {
+        navigate("/sign-in");
+      }, 1000);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <Toaster />
+      <div className="max-w-md w-full bg-white shadow-md rounded-lg p-6 space-y-4">
+        <h2 className="text-2xl font-bold text-center">Sign Up</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Username
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              type="text"
+              id="username"
+              value={formData.username || ""}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              type="email"
+              id="email"
+              value={formData.email || ""}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              type="password"
+              id="password"
+              value={formData.password || ""}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+
+          <div>
+            <button
+              disabled={loading}
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              {loading ? "Loading..." : "Sign Up"}
+            </button>
+          </div>
+        </form>
+        <p className="text-sm text-center text-gray-600">
+          Already have an account?{" "}
+          <Link to="/sign-in" className="text-indigo-600 hover:text-indigo-500">
+            Log In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
